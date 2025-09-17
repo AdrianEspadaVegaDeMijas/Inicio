8495dc1d082f2d206ed56948d8df12a0bb850a4e
AdrianEspadaVegaDeMijas <aespcer@g.educaand.es>
2025-09-17 19:15:18 +0200

    EJercicio E Terminado

--- src/Ejercicios/EjercicioE.java
+++ src/Ejercicios/EjercicioE.java
@@ -13,19 +13,31 @@ public class EjercicioE {
 
         int cont = n-1;
 
-        for(int i = 0; i < n*2; i = i + 2) {
+        for(int i = 0; i < n; i++) {
 
+
+            //calcula el salto del centro
+
+            //Espacio a la izquierda
             for(int j = 0; j < cont; j++) {
+            
                 System.out.print(" ");
-                
+            
             }
+
+            //Contador de espacios a la izquierda
             cont--;
 
-            for(int j = 0; j < 6 + i; j++) {
+           for(int j = 0; j < 6 + i + i; j++) {
 
-                System.out.print("*");
+                if(i % 2 != 0 && j > i && j <= i + 4) {
+                    System.out.print(" ");
+                }else {
+                    System.out.print("*");
+                }
 
             }
+
 
 
             System.out.println();
